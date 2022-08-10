65c3857e8d48d3da64b2b3d5d6c72e2da1aac870
kyuhun sim <kyuhunsim@gmail.com>
2022-08-10 14:15:26 +0900

    main page 세팅 준비

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from http.client import REQUEST_TIMEOUT
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(
+        request,
+        'blog/index.html',
+    )
