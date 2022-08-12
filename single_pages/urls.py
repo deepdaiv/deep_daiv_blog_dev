c754676ae23c2cd907a73109c967dfcd7e301647
Jiyoongee <109740391+Jiyoongee@users.noreply.github.com>
2022-08-12 13:57:19 +0900

    세부 페이지 만들기

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.landing),
+    path('',views.about_us),
+    path('',views.post),
+    path('',views.paper_review),
+    path('',views.wiki),
+    path('',views.study),
+    path('',views.event),
 
 ]
