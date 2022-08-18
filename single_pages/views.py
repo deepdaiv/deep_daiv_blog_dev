4c802424b74c9aec30440ecee4c07b98e4d09ebc
kyuhun sim <kyuhunsim@gmail.com>
2022-08-18 14:00:05 +0900

    병합 준비

--- single_pages/views.py
+++ single_pages/views.py
@@ -9,3 +9,38 @@ def landing(request):
         request,
         'single_pages/landing.html'
     )
+def about_us(request):
+    return render(
+        request,
+        'single_pages/about_us.html'
+    )
+
+def post(request):
+    return render(
+        request,
+        'single_pages/post.html'
+    )
+
+def paper_review(request):
+    return render(
+        request,
+        'single_pages/paper_review.html'
+    )
+
+def wiki(request):
+    return render(
+        request,
+        'single_pages/wiki.html'
+    )
+
+def study(request):
+    return render(
+        request,
+        'single_pages/study.html'
+    )
+
+def event(request):
+    return render(
+        request,
+        'single_pages/event.html'
+    )
