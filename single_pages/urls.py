4784ad428b2459d2859bd4c025fb958c20d90ad5
kyuhun sim <kyuhunsim@gmail.com>
2022-08-18 14:45:57 +0900

    메인페이지 작동완료

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.landing),
-    path('',views.about_us),
-    path('',views.post),
-    path('',views.paper_review),
-    path('',views.wiki),
-    path('',views.study),
-    path('',views.event),
+    path('about_us/',views.about_us),
+    path('post/',views.post),
+    path('paper_review/',views.paper_review),
+    path('wiki/',views.wiki),
+    path('study/',views.study),
+    path('event/',views.event),
 ]
