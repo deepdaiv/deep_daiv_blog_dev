1bac797cc31c187519e3394a1225783f677a121a
Seyoung1220 <ihtmy12@gmail.com>
2022-09-24 01:26:03 +0900

    edit urls.py

--- deepdaiv_blog_dev/urls.py
+++ deepdaiv_blog_dev/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
     path('',include('single_pages.urls'))
-    path('', views.index, name='index')
 ]
